36a13f23ba2441b690e406752934f829ffa9d4c3
Tunji-L <oyetunjilukman@gmail.com>
2022-03-11 22:14:56 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
+/**
+ * main - Printing to the screen with put command
+ *
+ * Return: 0 to end the code
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
