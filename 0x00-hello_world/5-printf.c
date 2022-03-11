079d3b5749a83b3032b26298bf58a97a072427d8
Tunji-L <oyetunjilukman@gmail.com>
2022-03-11 22:20:48 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing to the screen with printf command
+ *
+ * Return: 0 to end the code
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
