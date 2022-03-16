b187079842565bc8b8654fceebfab449008da1c1
Tunji-L <oyetunjilukman@gmail.com>
2022-03-16 17:51:06 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Check if character is in lower case
+ * 
+ * Return: true - 1, false - 0;
+ */
+int _islower(int c)
+{
+	int str;
+
+	if (c < 97 || c > 122)
+		str = _putchar(48);
+	str = _putchar(49);
+	return (str);
+}
