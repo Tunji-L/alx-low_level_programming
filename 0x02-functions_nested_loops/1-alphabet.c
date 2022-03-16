5ed797e35bac2c29eef7aa13311d40a266e71d66
Tunji-L <oyetunjilukman@gmail.com>
2022-03-16 12:38:52 +0000

    Print out alphabet in Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Alphabet in lowercase and print newline
+ *
+ * Return: 0 for success
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i < 'z'; i++)
+		_putchar(i);
+	_putchar(10);
+	return;
+}
