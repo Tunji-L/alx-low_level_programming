f07e8c4c43c5b82009ba977f9c64425e2ccbba1a
Tunji-L <oyetunjilukman@gmail.com>
2022-03-16 04:08:20 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Printing _putchar
+ *
+ * Return - The program should return 0
+ *
+ */
+
+int main(void)
+{
+	char s[] = "_putchar\n";
+
+	write(2, s, 9);
+	return (0);
+}
