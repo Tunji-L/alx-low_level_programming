9f96ab496cc224aa2998a3b6753403fe3a245855
Tunji-L <oyetunjilukman@gmail.com>
2022-03-17 05:42:32 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - print absolute value
+ *
+ * @n: Input character
+ *
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1 * n);
+	else
+		return (n);
+}
