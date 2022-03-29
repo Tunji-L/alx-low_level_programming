2981900a3ef165c2c6c49c15d4a2ae777f03b0b9
Tunji-L <oyetunjilukman@gmail.com>
2022-03-29 15:46:21 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a substring.
+ * @s: Input String.
+ * @c: Input char.
+ * Return: char address
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) != c)
+			i++;
+		else
+			break;
+	}
+	return (s + i);
+}
