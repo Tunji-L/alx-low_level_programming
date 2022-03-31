361838b5e1c32c85d7d71e15808c43f9bac6f81e
Tunji-L <oyetunjilukman@gmail.com>
2022-03-31 04:38:49 +0000

    length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,7 +8,8 @@ int _strlen_recursion(char *s)
 {
 	int len;
 
-	if (*s != '\0')
-		len = (1 + _strlen_recursion(s + 1));
-	return (len);
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
