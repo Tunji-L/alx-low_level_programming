ccf6fae069ac12b01dc582d9feace4529433df78
Tunji-L <oyetunjilukman@gmail.com>
2022-04-09 16:58:21 +0000

    prints a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle -  draws a diagonal line on the terminal
+ * @size: Length of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size < 1)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size; j > i; j--)
+				_putchar(32);
+			for (k = 0; k <= i; k++)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+}
