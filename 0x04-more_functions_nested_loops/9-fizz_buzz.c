e588fd5e8a9d6cfdc3c430fd9243444912133e8e
Tunji-L <oyetunjilukman@gmail.com>
2022-04-09 14:34:09 +0000

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - Fizz-Buzz test
- * Retain: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -19,5 +19,5 @@ int main(void)
 			printf("%d ", i);
 	}
 	printf("\n");
-	return 0;
+	return (0);
 }
