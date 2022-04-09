5f81f11022e485a5fcb540404eef92ea62ebe0d1
Tunji-L <oyetunjilukman@gmail.com>
2022-04-09 19:35:30 +0000

    prints the largest prime factor of the number 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - largest prime factor of the number 612852475143
+ * Return: Always 0
+ */
+int main(void)
+{
+	int j;
+	long int i = 612852475143;
+
+	while (i > 1)
+	{
+		for (j = 2; j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				i = i / j;
+				break;
+			}
+		}
+	}
+	printf("%d ", j);
+	printf("\n");
+	return (0);
+}
