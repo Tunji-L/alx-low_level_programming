e0823d60ce1e06e8fe8d39e2d1fa01d07f694541
Tunji-L <oyetunjilukman@gmail.com>
2022-04-09 12:46:21 +0000

    draws a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - Print Line of n
+ * @n: number of line to print
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(95);
+	_putchar('\n');
+}
