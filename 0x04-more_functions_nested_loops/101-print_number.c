be5832edeb54c78c9944e6793bff387efa4adfba
Tunji-L <oyetunjilukman@gmail.com>
2022-04-10 07:39:44 +0000

    prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - function that prints an integer
+ * @n: Integer to be printed
+ */
+void print_number(int n)
+{
+	int count, i, j, k, l;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	count = 0;
+	k = n;
+	while (k / 10)
+	{
+		k = k / 10;
+		count++;
+	}
+	while (count >= 0)
+	{
+		j = count;
+		l = n;
+		for (i = 1; i <= j; i++)
+		{
+			l = l / 10;
+		}
+		_putchar((l % 10) + '0');
+		count--;
+	}
+}
