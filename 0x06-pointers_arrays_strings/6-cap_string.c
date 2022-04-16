68d4c79999d029cd0c87ef8b516cd0082b9a07f0
Tunji-L <oyetunjilukman@gmial.com>
2022-04-16 16:16:27 +0000

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,10 +7,10 @@
 char *cap_string(char *str)
 {
 	int i, j, k;
-	int ss[] = {9,10,32,44,59,46,33,63,34,40,41,123,125};
+	int ss[] = {9, 10, 32, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	j = 0;
-	
+
 	while (str[j] != '\0')
 		j++;
 
@@ -19,7 +19,7 @@ char *cap_string(char *str)
 		for (k = 0; k < 13; k++)
 		{
 			if (str[i] == ss[k] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-		       	{
+			{
 				str[i + 1] = str[i + 1] - 32;
 			}
 		}
