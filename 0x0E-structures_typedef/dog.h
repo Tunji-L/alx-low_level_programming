8bc4584aa1a2daf749348050f752358135326e92
Tunji-L <oyetunjilukman@gmial.com>
2022-04-25 09:25:11 +0000

    new type dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
