8dff109a6586416d6c4875ed2c2320483c74c261
Tunji-L <oyetunjilukman@gmial.com>
2022-04-29 14:54:23 +0000

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,9 +22,11 @@ int _pow(int x, int y)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
+	int i, j = 0;
 	unsigned int sum = 0;
 
+	if (!b)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == 48 || b[i] == 49)
@@ -32,8 +34,6 @@ unsigned int binary_to_uint(const char *b)
 		else
 			return (0);
 	}
-
-	int j = 0;
 
 	while (i > 0)
 	{
