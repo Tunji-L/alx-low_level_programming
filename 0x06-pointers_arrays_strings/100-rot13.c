4dfb219083cd994bca23df895089a1f71b581242
Tunji-L <oyetunjilukman@gmial.com>
2022-06-23 17:42:10 +0000

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,17 +7,22 @@
 
 char *rot13(char *s)
 {
-	int i = 0;
+	int i = 0, j;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= 65 && s[i] <= 77) || (s[i] >= 97 && s[i] <= 109))
-			s[i] = (s[i] + 13);
-		else if ((s[i] >= 78 && s[i] <= 90) || (s[i] >= 110 && s[i] <= 122))
-			s[i] = s[i] - 13;
-		else
-			s[i] = s[i];
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
 		i++;
 	}
+
 	return (s);
 }
